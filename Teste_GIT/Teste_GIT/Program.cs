c30c2793052460d6bed2e58be6c235d40c377bdf
filipe-malainho <filipemalainho@gmail.com>
2024-09-11 10:40:20 +0100

    add duas linhas de codigo
    add das linhas de codigo no primeiro programa

---
+++ Teste_GIT/Teste_GIT/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Teste_GIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("linha de teste");
+            Console.WriteLine("introdução de função de cálculo");
+        }
+    }
+}
